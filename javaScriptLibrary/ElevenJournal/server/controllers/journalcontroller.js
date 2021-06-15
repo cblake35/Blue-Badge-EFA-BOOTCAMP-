9895fcb047b09286ab2d3775bf35d3c0cbc8f1b6
Chris Blake <c.blake@Chriss-Air.attlocal.net>
2021-06-14 21:32:49 -0400

    Added client side for CRUD operations

--- javaScriptLibrary/ElevenJournal/server/controllers/journalcontroller.js
+++ javaScriptLibrary/ElevenJournal/server/controllers/journalcontroller.js
@@ -1,14 +1,122 @@
 const Express = require("express");
 const router = Express.Router();
 //injecting the validateJWT variable directly is best when we have a controller where a specific number of the routes need to be restricted
-let validateJWT = require("../middleware/validate-jwt"); 
+let validateJWT = require("../middleware/validate-jwt");
+const { JournalModel } = require("../models");
 
 router.get('/practice', validateJWT, (req, res) => {
     res.send('Hey! this is a practice route!')
 });
+
+router.post("/create", validateJWT, async (req, res) => {
+    const { title, date, entry } = req.body.journal;
+    const { id } = req.user;
+    const journalEntry = {
+        title,
+        date,
+        entry,
+        owner: id
+    }
+    try {
+        const newJournal = await JournalModel.create(journalEntry);
+        res.status(200).json(newJournal);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+
+router.get("/", async (req, res) => {
+    try {
+        const entries = await JournalModel.findAll();
+        res.status(200).json(entries);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+
+router.get("/mine", validateJWT, async (req, res) => {
+    const { id } = req.user;
+    try {
+        const userJournals = await JournalModel.findAll({
+            where: {
+                owner: id
+            }
+        });
+        res.status(200).json(userJournals);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+
+router.get("/:title", async (req, res) => {
+    const { title } = req.params;
+    try {
+        const results = await JournalModel.findAll({
+            where: { title: title }
+        });
+        res.status(200).json(results);
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+})
+
+router.put("/update/:entryId", validateJWT, async (req, res) => {
+    const { title, date, entry } = req.body.journal;
+    const journalId = req.params.entryId;
+    const userId = req.user.id;
+
+    const query = {
+        where: {
+            id: journalId,
+            owner: userId
+        }
+    };
+
+    const updatedJournal = {
+        title: title,
+        date: date,
+        entry: entry
+    };
+
+    try {
+        const update = await JournalModel.update(updatedJournal, query);
+        res.status(200).json(update);
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+})
+
+router.delete("/delte/:id", validateJWT, async (req, res) => {
+    const ownerId = req.user.id;
+    const journalId = req.params.id;
+
+    try {
+        const query = {
+            where: {
+                id: journalId,
+                owner: ownerId
+            }
+        };
+
+        await JournalModel.destroy(query);
+        res.status(200).json({ message: "Journal entry removed" });
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+})
 
 router.get('/about', (req, res) => {
     res.send('This is the about route')
 });
 
 module.exports = router;
+
+
+// {
+//     "journal":
+//     {
+//         "title": "Escapade",
+//         "date": "03/03/2020",
+//         "entry": "Who let the ddogs out?"
+//     }
+// }
