a3cd6bbb1d34d09ec680de41d9121309548a3759
Chris Blake <c.blake@Chriss-Air.attlocal.net>
2021-06-18 19:45:56 -0400

    Added functionality to display journals(own and others)

--- javaScriptLibrary/ElevenJournal/client/post-edit-delete-scripts.js
+++ javaScriptLibrary/ElevenJournal/client/post-edit-delete-scripts.js
@@ -2,21 +2,51 @@
  *** POST JOURNAL ***
 ************************** */
 function postJournal() {
-     console.log('postJournal Function Called')
+    //  console.log('postJournal Function Called')
+    const accessToken = localStorage.getItem('SessionToken')
+    let title = document.getElementById('title').value;
+    let date = document.getElementById('date').value;
+    let entry = document.getElementById('entry').value;
+
+    let newEntry = {
+        journal: {
+            title: title,
+            date: date,
+            entry: entry
+        }
     }
-    
-    
-    /* *************************
-     *** UPDATE JOURNAL ***
-    ************************** */
-    function editJournal(postId) {
-     console.log('editJournal Function Called')
-    }
-    
-    
-    /* *************************
-     *** DELETE JOURNAL ***
-    ************************** */
-    function deleteJournal(postId) {
-     console.log('deleteJournal Function Called')
-    }
+
+    fetch('http://localhost:3000/journal/create', {
+        method: 'POST',
+        headers: new Headers({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        }),
+        body: JSON.stringify(newEntry)
+
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            displayMine()
+        })
+        .catch(err => {
+            console.errorErr
+        })
+}
+
+
+/* *************************
+ *** UPDATE JOURNAL ***
+************************** */
+function editJournal(postId) {
+    console.log('editJournal Function Called')
+}
+
+
+/* *************************
+ *** DELETE JOURNAL ***
+************************** */
+function deleteJournal(postId) {
+    console.log('deleteJournal Function Called')
+}
