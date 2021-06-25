ffc0aacc1dc9ef8f311c4f272d2268d3f688d1ca
Chris Blake <c.blake@Chriss-Air.attlocal.net>
2021-06-24 21:29:14 -0400

    Added a new challenge and notes regarding react

---
+++ Challenges/react-challenge/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react'
+import Input from './components/Inputs';
+import View from './components/View';
+
+function App() {
+
+  const [name, setName] = useState('enter name');
+  const [age, setAge] = useState('enter age');
+  const [hobbies, setHobbies] = useState('enter hobby');
+
+  return (
+    <div className="App">
+      <Input
+        age={age}
+        name={name}
+        hobbies={hobbies}
+        setName={setName}
+        setAge={setAge}
+        setHobbies={setHobbies}
+      />
+      <View
+        passedName={name}
+        age={age}
+        hobbies={hobbies}
+      />
+    </div>
+  );
+}
+
+export default App;
